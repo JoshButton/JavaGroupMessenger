b8e8d44ddda205cb3a86e0f6a718ae8a43715d6a
JoshButton <34740880+JoshButton@users.noreply.github.com>
2018-01-11 02:00:32 +0000

    Update, Final?
    Add JUnit Test

---
+++ test/JUnitTest.java
@@ -0,0 +1,17 @@
+
+import org.junit.Test;
+
+public class JUnitTest {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        Database tester = new Database(); // MyClass is tested
+
+        @Test
+        public void test1
+
+            () {
+            assertFalse(tester.isOdd(2));
+        }
+    }
+}
